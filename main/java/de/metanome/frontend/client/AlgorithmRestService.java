1f24ade23c996cf3be94a9c1f76c64b1bb47fb84
Tanja Bergmann <tabergma@gmail.com>
2014-11-28 15:22:15 +0100

    Restful example is working: BasePage calls AlgorithmRes via rest service.

---
+++ main/java/de/metanome/frontend/client/AlgorithmRestService.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2014 by the Metanome project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package de.metanome.frontend.client;
+
+
+import de.metanome.backend.results_db.AlgorithmObj;
+
+import org.fusesource.restygwt.client.MethodCallback;
+import org.fusesource.restygwt.client.RestService;
+
+import java.util.List;
+
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+
+@Path("/api/algorithms")
+public interface AlgorithmRestService extends RestService {
+
+  @GET
+  public void getAlgorithms(MethodCallback<List<AlgorithmObj>> callback);
+
+}
